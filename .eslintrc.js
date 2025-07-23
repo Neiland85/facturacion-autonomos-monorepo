fc30938ee95667779f0a60d40f7e66abe808c87c
Neiland85 <theia3impact@gmail.com>
2025-07-23 05:04:21 +0200

    fix: ajustes para despliegue en Netlify

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  parserOptions: {
+    // ...existing code...
+    project: [
+      "./apps/web/tsconfig.json",
+      // ...existing code...
+    ]
+  },
+  // ...existing code...
+};
