eda20a786028edaa6fa6ca857e6c6a33b2db6845
Neil Munoz <theia3impact@gmail.com>
2025-10-04 18:53:01 +0200

    🔧 Fix Jest configuration: Correct import path for base config

--- apps/web/jest.config.js
+++ apps/web/jest.config.js
@@ -1,5 +1,8 @@
 const nextJest = require('next/jest');
-const { nextConfig } = require('../jest.config.base.js');
+const path = require('path');
+
+// Importar configuración base desde el directorio raíz
+const baseConfig = require(path.join(__dirname, '../../jest.config.base.js'));
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files
@@ -8,8 +11,10 @@ const createJestConfig = nextJest({
 
 // Add any custom config to be passed to Jest
 const customJestConfig = {
-  ...nextConfig,
+  ...baseConfig,
   // Configuración específica adicional para web si es necesaria
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
